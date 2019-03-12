947579f69843d8da3d9651532013cde7953aa7a4
nina <testni2312@gmail.com>
2019-03-12 17:58:41 +0200

    Test case on forgot password function
    This is positive test case for forgot password function.
    After inputting valid data test should be passed

---
+++ oct/pages/forgot_password.py
@@ -0,0 +1,40 @@
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver import Remote
+from oct.pages.base import Page
+from oct.pages.registration import PersonalDetails
+
+
+class ForgotPasswordPage:
+    def __init__(self, browser: Remote):
+        self._browser = browser
+        self._users_email = PersonalDetails(browser)
+
+    def open(self) -> None:
+        self._browser.get("https://localhost/index.php?route=account/forgotten")
+        self._browser.implicitly_wait(10)
+
+    def loaded(self) -> bool:
+        return "Forgot Your Password?" in self._browser.title
+
+    def fill_email(self, email: str) -> None:
+        self._users_email.type_email(email)
+
+    def press_continue_button(self) -> None:
+        self._browser.find_element_by_xpath("//*[@id='content']/form/div/div[2]/input").click()
+
+
+class ConfirmationMessage(Page):
+    def __init__(self, browser: Remote):
+        self._browser = browser
+        self._browser.implicitly_wait(10)
+
+    def open(self) -> None:
+        self._browser.get("https://localhost/index.php?route=account/account")
+        self._browser.implicitly_wait(10)
+
+    def loaded(self) -> bool:
+        try:
+            self._browser.find_element_by_css_selector("div.alert.alert-success.alert-dismissible")
+            return True
+        except NoSuchElementException:
+            return False
