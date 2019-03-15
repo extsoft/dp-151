4405595b05fb45654c666a755b324f4678ae6f76
Aleksandr <bambula4000@gmail.com>
2019-03-15 17:11:20 +0200

    Share test case has been created.
    Added share.py module in oct/pages for implementig share object on the product
    page.
    Added share.py module in oct/tests/web for implementing and running test case
    which verifies ability to open share popup window on the product page.

---
+++ oct/pages/share.py
@@ -0,0 +1,14 @@
+from selenium.webdriver import Remote
+from selenium.common.exceptions import NoSuchElementException
+
+
+class ShareWindow:
+    def __init__(self, browser: Remote):
+        self._browser = browser
+
+    def is_open_popup_share(self) -> bool:
+        try:
+            self._browser.find_element_by_xpath('//*[@id="at-expanded-menu-container"]/div[2]')
+            return True
+        except NoSuchElementException:
+            return False
