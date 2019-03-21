e1c660042a0dc9827fdeac71864aeb3d92c6f495
nina <testni2312@gmail.com>
2019-03-21 19:08:13 +0200

    Test cases on forgot password functional was edited
    1. In file forgot_password.py was deleted one extra method
    2. In file forgot_password_test.py two tests were merged in one class
    3. In negative test case was changed assertation logic

--- oct/tests/web/forgot_password_test.py
+++ oct/tests/web/forgot_password_test.py
@@ -16,6 +16,15 @@ class ForgotPassword(Testcase):
         forgot_password.press_continue_button()
         assert ConfirmationMessage(chrome).loaded()
 
+    @test
+    def test_forgot_password_with_not_existing_email(self, grid: str) -> None:
+        chrome: Remote = Chrome(grid)
+        forgot_password = ForgotPasswordPage(chrome)
+        forgot_password.open()
+        forgot_password.fill_email("testerwom777@gmail.com")
+        forgot_password.press_continue_button()
+        assert not ConfirmationMessage(chrome).loaded()
+
 
 if __name__ == "__main__":
     run_testcase()
