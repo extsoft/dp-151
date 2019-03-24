4f889b00ba377bc121d139a4831f694a7a2b1a77
Yehor Koval'ov <bingobingo1997@gmail.com>
2019-03-24 16:38:59 +0200

    Add Deploy_app test
    In testbed.yml described information about connecting to the server.
    Added docker-compose.yaml.
    Created file deploy_app.py with test. This test checks whether the application is deployed on the server. #23

---
+++ oct/tests/deployment/deploy_app.py
@@ -0,0 +1,48 @@
+# pylint: disable=no-self-use # pyATS-related exclusion
+# pylint: disable=no-member
+# pylint: disable=invalid-name
+# pylint: disable=too-many-branches
+import os
+from pyats.topology import Device, loader
+from pyats.aetest import Testcase, test
+from pyats.utils.fileutils import FileUtils
+from requests.exceptions import ConnectTimeout
+from oct.tests import run_testcase
+
+
+def copy_file_to_server(device: Device) -> None:
+    futils = FileUtils(testbed=device.testbed)
+    futils.copyfile(
+        source=f"{os.path.join(os.getcwd(), 'docker-compose.yaml')}",
+        destination=f"scp://{device.connections.main.ip}/home/adminaccount/oct",
+    )
+
+
+def is_deploy_app(device: Device) -> bool:
+    non_empty_lines_counter = 0
+    for iterator in device.execute("docker-compose ps -q"):
+        if iterator == "\n":
+            non_empty_lines_counter += 1
+    return non_empty_lines_counter == 2
+
+
+class DeployApp(Testcase):
+    testbed = loader.load("testbed.yaml")
+
+    @test
+    def deploy_app(self, device: Device = testbed.devices["vm"]) -> None:
+        try:
+            device.connect()
+            device.execute("mkdir oct")
+            copy_file_to_server(device)
+            device.execute("cd oct")
+            device.execute(f"APP_HOST={device.connections.main.ip} docker-compose up -d")
+            assert is_deploy_app(device)
+        except ConnectTimeout:
+            self.failed()
+        finally:
+            device.disconnect()
+
+
+if __name__ == "__main__":
+    run_testcase()
