711d8ded1c8c89a1b2a8f6e107c5ecc3979f4585
Aleksey.D <didilov.aleksey@gmail.com>
2019-03-25 18:32:46 +0200

    Add logic which allows run 'Login' test case on new environment
    Login test case was implemented for checking ability to login into
    user account(with valid credentials and forbidden login if credentials
    incorrect).
    
    If user wants to run this test case on fresh enviroment, need to
    create new account for ability to do this. In this case setup section
    was implemented for web and api tests. This section allows creating new
    account, using test case registration via API, if it wasn't done before,
    if user will run login test case second time, setup section will create
    new user..
    It is connected with class EmailGeneration, which creates new email
    each time. #26
    
    'Registration' test case was updated. Logic which needed to send http
    request with post method was moved in oct/test/api/registration_patterm.py
    Logic which implemented in this module, will be used for registration
    api test case and for setup method in oct/test/web/login.

---
+++ oct/tests/api/registration_pattern.py
@@ -0,0 +1,58 @@
+import urllib3
+import requests
+
+
+class Identity:
+    def __init__(self, first_name: str, last_name: str, telephone: str):
+        self._first_name = first_name
+        self._last_name = last_name
+        self._telephone = telephone
+
+    def get_first_name(self) -> str:
+        return self._first_name
+
+    def get_last_name(self) -> str:
+        return self._last_name
+
+    def get_telephone(self) -> str:
+        return self._telephone
+
+
+class Credentials:
+    def __init__(self, email: str, password: str, news_letter: str):
+        self._email = email
+        self._password = password
+        self._news_letter = news_letter
+
+    def get_email(self) -> str:
+        return self._email
+
+    def get_password(self) -> str:
+        return self._password
+
+    def get_newsletter(self) -> str:
+        return self._news_letter
+
+
+class UserRegistration:
+    def __init__(self, identity: Identity, credentials: Credentials):
+        self._identity = identity
+        self._credentials = credentials
+
+    def registration_response(self, domain_name: str) -> str:
+        urllib3.disable_warnings()
+        return requests.post(
+            f"https://{domain_name}//index.php?route=account/register",
+            {
+                "customer_group_id": "1",
+                "firstname": self._identity.get_first_name(),
+                "lastname": self._identity.get_last_name(),
+                "email": self._credentials.get_email(),
+                "telephone": self._identity.get_telephone(),
+                "password": self._credentials.get_password(),
+                "confirm": self._credentials.get_password(),
+                "newsletter": self._credentials.get_newsletter(),
+                "agree": "1",
+            },
+            verify=False,
+        ).url
--- oct/tests/web/login.py
+++ oct/tests/web/login.py
@@ -1,21 +1,50 @@
 # pylint: disable=no-self-use # pyATS-related exclusion
-from pyats.aetest import Testcase, test
+from pyats.aetest import Testcase, test, setup
 from selenium.webdriver import Remote
 from oct.browsers import Chrome
 from oct.tests import run_testcase
 from oct.pages.login import LoginPage
 from oct.pages.account import AccountPage
+from oct.tests.api.registration_pattern import UserRegistration, Identity, Credentials
+from oct.tests.web.creating_emails import EmailsGeneration
 
 
 class Login(Testcase):
+    email = EmailsGeneration().creating_full_email()
+    password = "12345678"
+
+    @setup
+    def create_account(self) -> None:
+        assert "success" in UserRegistration(
+            Identity("Alex", "Ivanov", "+38090890812"), Credentials(self.email, self.password, "0")
+        ).registration_response("localhost")
+
     @test
     def login_exists_credentials(self, grid: str) -> None:
         chrome: Remote = Chrome(grid)
         login = LoginPage(chrome)
         login.open()
-        login.fill_credentials("didilov.aleksey@gmail.com", "12345678")
+        login.fill_credentials(self.email, self.password)
         login.press_login_button()
         assert AccountPage(chrome).loaded()
+
+    @test
+    def login_nonexistent_email(self, grid: str) -> None:
+        chrome: Remote = Chrome(grid)
+        login = LoginPage(chrome)
+        login.open()
+        login.fill_credentials("Er12asd@mailinator.com", self.password)
+        login.press_login_button()
+        assert not AccountPage(chrome).loaded()
+
+    @test
+    def login_nonexistent_password(self, grid: str) -> None:
+        chrome: Remote = Chrome(grid)
+        login = LoginPage(chrome)
+        login.open()
+        login.fill_credentials(self.email, "12345789")
+        login.press_login_button()
+        assert not AccountPage(chrome).loaded()
 
 
 if __name__ == "__main__":
