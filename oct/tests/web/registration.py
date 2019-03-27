336c3ea7d6e0d696dfe4342863f1d30827c05467
nina <testni2312@gmail.com>
2019-03-27 11:57:04 +0200

    In test case web/registration.py was used random email generator instead of static email
    Test case of registration is using method which helps to create random exceptional email.
    It was created for using this test case several times without changing static email.
    Was deleted import time.

--- oct/tests/web/registration.py
+++ oct/tests/web/registration.py
@@ -1,11 +1,10 @@
 # pylint: disable=no-self-use # pyATS-related exclusion
-
-
 from pyats.aetest import Testcase, test
 from selenium.webdriver import Remote
 from oct.browsers import Chrome
 from oct.tests import run_testcase
 from oct.pages.registration import RegisterAccountPage, RegistrationSuccessPage
+from oct.tests.web.creating_emails import EmailsGeneration
 
 
 class Registration(Testcase):
@@ -13,8 +12,11 @@ class Registration(Testcase):
     def test(self, grid: str) -> None:
         chrome: Remote = Chrome(grid)
         registration = RegisterAccountPage(chrome)
+        random_email = EmailsGeneration()
         registration.open()
-        registration.fill_personal_details("Jon", "Doe", "test2@sdf.com", "123456")
+        registration.fill_personal_details(
+            "Jon", "Doe", random_email.creating_full_email(), "123456"
+        )
         registration.fill_password("supersecret")
         registration.press_continue()
         assert RegistrationSuccessPage(chrome).loaded()
