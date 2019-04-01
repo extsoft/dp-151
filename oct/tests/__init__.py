7409718a2d23803ab5c2b0d618e91507336a9b5a
Yehor Koval'ov <bingobingo1997@gmail.com>
2019-04-01 22:13:58 +0300

    Change of logic in function deploy_app and mandatory_aetest_arguments
    In the function deploy_app, the loading of the testbed file was removed.
    In the mandatory_aetest_argumets was added device. This helped pass the device argument to deploy_app.
    In the suite.py was modifified main function

--- oct/tests/__init__.py
+++ oct/tests/__init__.py
@@ -1,15 +1,18 @@
+# pylint: disable=no-member
+# pylint: disable=no-name-in-module
 from typing import Dict, Any
-
 from pyats import aetest
 from pyats.topology import loader
-from pyats.topology.testbed import Testbed  # pylint: disable=no-name-in-module
+from pyats.topology.testbed import Testbed
 
 
-def mandatory_aetest_arguments(testbed: Testbed,) -> Dict[str, Any]:
-    return {"testbed": testbed, "grid": testbed.custom["selenium-grid"]}
+def mandatory_aetest_arguments(testbed: Testbed, device_name: str) -> Dict[str, Any]:
+    return {
+        "testbed": testbed,
+        "grid": testbed.custom["selenium-grid"],
+        "device": testbed.devices[device_name],
+    }
 
 
-def run_testcase(testbed_file: str = "testbed.yaml") -> None:
-    aetest.main(
-        **mandatory_aetest_arguments(loader.load(testbed_file))  # pylint: disable=no-member
-    )
+def run_testcase(testbed_file: str = "testbed.yaml", device_name: str = "vm") -> None:
+    aetest.main(**mandatory_aetest_arguments(loader.load(testbed_file), device_name))
