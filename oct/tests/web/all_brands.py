ca77bc9be7ef418c360b72b9b32d3f6bdabb6119
Mr. Green <greenpasha22@gmail.com>
2019-04-01 15:32:26 +0300

    Implemented Brands page and all_brands test
    The page with all brands are added in this commit, and test which check redirecting to the apple brand page from the all brands page are implemented. We can change apple to any brand using different numbers in our test.

---
+++ oct/tests/web/all_brands.py
@@ -0,0 +1,21 @@
+# pylint: disable=no-self-use # pyATS-related exclusion
+from pyats.aetest import Testcase, test
+from selenium.webdriver import Remote
+from oct.tests import run_testcase
+from oct.browsers import Chrome
+from oct.pages.brand import BrandPage
+from oct.pages.brands import BrandsPage, BrandList
+
+
+class AllBrands(Testcase):
+    @test
+    def test(self, grid: str) -> None:
+        chrome: Remote = Chrome(grid)
+        brands_page = BrandsPage(chrome)
+        brands_page.open()
+        brands_page.click_brand_name(BrandList.APPLE)
+        assert BrandPage(chrome, "8", "Apple").loaded()
+
+
+if __name__ == "__main__":
+    run_testcase()
