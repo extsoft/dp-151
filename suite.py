9e5061fac7b69c06fc9c5fe2e36b60c6938ee6be
Flower1111 <kryva.liliia@gmail.com>
2019-04-03 15:32:26 +0200

    Added modules for running all testcases to suite.py.
    Missing modules were imported from api package, web package, deployment package to suite.py.
    Missing modules were added to _api_tests, _web_tests, _deployment_tests in suite.py.

--- suite.py
+++ suite.py
@@ -8,15 +8,15 @@ from pyats import easypy
 from pyats.easypy.main import EasypyRuntime
 
 from oct.tests import mandatory_aetest_arguments
-from oct.tests.web import sample as web_sample_test
 from oct.tests.web import adding_to_cart
 from oct.tests.web import adding_to_wish_list
 from oct.tests.web import affiliate_register as web_affiliate_register_test
+from oct.tests.web import all_brands
 from oct.tests.web import brand
 from oct.tests.web import contact_us as web_contact_us_test
 from oct.tests.web import creating_emails
 from oct.tests.web import forgot_password_test
-from oct.tests.web import gift_certificate
+from oct.tests.web import gift_certificate as web_gift_certificate_test
 from oct.tests.web import login as web_login_test
 from oct.tests.web import open_prod_img
 from oct.tests.web import registration as web_registration_test
@@ -26,25 +26,38 @@ from oct.tests.web import share
 from oct.tests.api import affiliate_register
 from oct.tests.api import contact_us
 from oct.tests.api import forgot_passwrd
+from oct.tests.api import gift_certificate
 from oct.tests.api import login
 from oct.tests.api import registration
+from oct.tests.api import registration_pattern
 from oct.tests.api import returns
 from oct.tests.api import review
 from oct.tests.deployment import deploy_app
+from oct.tests.deployment import destroy_app
 
 
-_api_tests = (affiliate_register, contact_us, forgot_passwrd, login, registration, returns, review)
+_api_tests = (
+    affiliate_register,
+    contact_us,
+    forgot_passwrd,
+    gift_certificate,
+    login,
+    registration,
+    registration_pattern,
+    returns,
+    review,
+)
 
 _web_tests = (
-    web_sample_test,
     adding_to_cart,
     adding_to_wish_list,
     web_affiliate_register_test,
+    all_brands,
     brand,
     web_contact_us_test,
     creating_emails,
     forgot_password_test,
-    gift_certificate,
+    web_gift_certificate_test,
     web_login_test,
     open_prod_img,
     web_registration_test,
@@ -53,7 +66,7 @@ _web_tests = (
     share,
 )
 
-_deployment_tests = (deploy_app,)
+_deployment_tests = (deploy_app, destroy_app)
 
 
 def tests_runner(test_suite: Tuple, instance: Any) -> List:  # type: ignore
