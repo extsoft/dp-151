bb291756f3cce63b2630468d21fafdb959f8af35
Oleksii Poliakov <opol2@macmini908.ddns.softservecom.com>
2019-05-30 11:18:03 +0300

    Optimized api tests
    Removed unnecessary variables
    Requests moved to assert
    Operations performed in the following tests:
    1.affiliate_register
    2.contact_us
    3.returns
    4.rewiew
    5.gift-certificate

--- oct/tests/api/review.py
+++ oct/tests/api/review.py
@@ -1,29 +1,25 @@
 # pylint: disable=no-self-use # pyATS-related exclusion
-import urllib3
-import requests
 from pyats.topology import Device
 from pyats.aetest import Testcase, test
 from oct.tests import run_testcase
+from oct.tests.api.affiliate_register import Post
+from oct.tests.api.returns import assert_if_request_contains_success_response_text
 
 
 class Review(Testcase):
     @test
     def test_review(self, device: Device) -> None:
-        params = {
-            "name": "Solomon",
-            "text": "This is auto test-bot review. This is auto test-bot review.",
-            "rating": "5",
-        }
-        urllib3.disable_warnings()
-        review_request = requests.post(
-            f"https://{device.connections.main.ip}/"
-            f"index.php?route=product/product/write&product_id=40",
-            params,
-            verify=False,
-        )
-        assert (
-            "Thank you for your review. It has been submitted to the webmaster for approval."
-            in review_request.text
+        assert_if_request_contains_success_response_text(
+            Post(
+                f"https://{device.connections.main.ip}/"
+                f"index.php?route=product/product/write&product_id=40",
+                {
+                    "name": "Solomon",
+                    "text": "This is auto test-bot review. This is auto test-bot review.",
+                    "rating": "5",
+                },
+            ),
+            "Thank you for your review. It has been submitted to the webmaster for approval.",
         )
 
 
